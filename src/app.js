21483e8aadc9e4c624431369f72e860d65d3586b
AJ <ajoeford@gmail.com>
2019-03-04 21:51:45 -0800

    heroku port

--- src/app.js
+++ src/app.js
@@ -11,19 +11,19 @@ app.use(express.json());
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
 })
-// app.post('/api/v1/expenses', Expense.create);
-// app.get('/api/v1/expenses', Expense.getAll);
-// app.get('/api/v1/expenses/:id', Expense.getOne);
-// app.put('/api/v1/expenses/:id', Expense.update);
-// app.delete('/api/v1/expenses/:id', Expense.delete);
+app.post('/api/v1/expenses', Expense.create);
+app.get('/api/v1/expenses', Expense.getAll);
+app.get('/api/v1/expenses/:id', Expense.getOne);
+app.put('/api/v1/expenses/:id', Expense.update);
+app.delete('/api/v1/expenses/:id', Expense.delete);
 
-// app.get('/api/v1/categories', Category.getAll);
-// app.post('/api/v1/categories', Category.create);
-// app.put('/api/v1/categories/:id', Category.update);
-// app.delete('/api/v1/categories/:id', Category.delete);
+app.get('/api/v1/categories', Category.getAll);
+app.post('/api/v1/categories', Category.create);
+app.put('/api/v1/categories/:id', Category.update);
+app.delete('/api/v1/categories/:id', Category.delete);
 
-// app.get('/api/v1/reports/recent', Expense.getRecentExpenses);
-// app.post('/api/v1/reports/monthly', Report.getMonthly);
+app.get('/api/v1/reports/recent', Expense.getRecentExpenses);
+app.post('/api/v1/reports/monthly', Report.getMonthly);
 
-app.listen(3001)
+app.listen(process.env.PORT || 3001);
 console.log('app running on port ', 3001);
