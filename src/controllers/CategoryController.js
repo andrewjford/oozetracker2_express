70e08761984a3d247aaa8c6b9cb423664485a7ee
AJ <ajoeford@gmail.com>
2019-03-30 10:45:33 -0700

    refactor to use category model

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -23,9 +23,8 @@ const CategoryController = {
   },
 
   async getOne(req, res) {
-    const text = 'SELECT * FROM categories WHERE id = $1 AND account_id = $2';
     try {
-      const { rows } = await db.query(text, [req.params.id, req.accountId]);
+      const { rows } = await CategoryModel.getOne(req);
       if (!rows[0]) {
         return res.status(404).send({'message': 'category not found'});
       }
@@ -36,21 +35,12 @@ const CategoryController = {
   },
 
   async update(req, res) {
-    const findOneQuery = 'SELECT * FROM categories WHERE id = $1 AND account_id = $2';
-    const updateOneQuery =`UPDATE categories
-      SET name = $1, modified_date = $2
-      WHERE id = $3 RETURNING *`;
     try {
-      const { rows } = await db.query(findOneQuery, [req.params.id, req.accountId]);
+      const { rows } = await CategoryModel.getOne(req);
       if(!rows[0]) {
         return res.status(404).send({'message': 'category not found'});
       }
-      const values = [
-        req.body.name || rows[0].name,
-        moment(new Date()),
-        req.params.id
-      ];
-      const response = await db.query(updateOneQuery, values);
+      const response = await CategoryModel.update(req, rows[0]);
       return res.status(200).send(response.rows[0]);
     } catch(err) {
       return res.status(400).send(err);
@@ -58,9 +48,8 @@ const CategoryController = {
   },
 
   async delete(req, res) {
-    const deleteQuery = 'DELETE FROM categories WHERE id = $1 AND account_id = $2 RETURNING *';
     try {
-      const { rows } = await db.query(deleteQuery, [req.params.id, req.accountId]);
+      const { rows } = await CategoryModel.delete(req);
       if(!rows[0]) {
         return res.status(404).send({'message': 'category not found'});
       }
--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -17,7 +17,32 @@ const CategoryModel = {
   getAll(req) {
     const findAllQuery = 'SELECT * FROM categories WHERE account_id = $1';
     return db.query(findAllQuery, [req.accountId]);
+  },
+
+  getOne(req) {
+    const text = 'SELECT * FROM categories WHERE id = $1 AND account_id = $2';
+    return db.query(text, [req.params.id, req.accountId]);
+  },
+
+  update(req, existingRecord) {
+    const updateOneQuery =`UPDATE categories
+      SET name = $1, modified_date = $2
+      WHERE id = $3 RETURNING *`;
+
+    const values = [
+      req.body.name || existingRecord.name,
+      moment(new Date()),
+      req.params.id
+    ];
+
+    return db.query(updateOneQuery, values);
+  },
+
+  delete(req) {
+    const deleteQuery = 'DELETE FROM categories WHERE id = $1 AND account_id = $2 RETURNING *';
+    return db.query(deleteQuery, [req.params.id, req.accountId]);
   }
+
 }
 
 export default CategoryModel;
