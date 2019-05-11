7a972fd5b80bdb14adc5a8540ac93978747c9f6c
AJ <ajoeford@gmail.com>
2019-05-11 10:39:14 -0700

    fix verification token sql

--- src/db/user_email_verification.sql
+++ src/db/user_email_verification.sql
@@ -3,6 +3,8 @@ ALTER TABLE accounts ADD COLUMN is_verified BOOLEAN;
 CREATE TABLE IF NOT EXISTS verification_tokens (
   id SERIAL PRIMARY KEY,
   token text NOT NULL, 
-  account_id REFERENCES accounts(id) NOT NULL;
-  created_date timestamptz NOT NULL DEFAULT NOW(),
+  account_id INTEGER REFERENCES accounts(id) NOT NULL,
+  created_date timestamptz NOT NULL DEFAULT NOW()
   );
+
+ALTER TABLE accounts ALTER COLUMN is_verified SET DEFAULT FALSE;
