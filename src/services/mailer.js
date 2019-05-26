cc22056ed1b9c0ef95948599a7216d9e56065c19
AJ <ajoeford@gmail.com>
2019-05-26 11:15:57 -0700

    update email template

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -8,8 +8,8 @@ const mailer = {
     sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: toEmail,
-      from: 'verify@oozetracker.com',
-      subject: 'Welcome to Green Track! Please Confirm Your Email',
+      from: 'Cash Tracker',
+      subject: 'Welcome to Cash Tracker! Please Confirm Your Email',
       text: `Click on this link to verify your email ${hostUrl}/verification?token=${token}&email=${toEmail}`,
       html: `<strong>
         Click <a href="http://${hostUrl}/api/v1/verification?token=${token}&email=${toEmail}">here</a> to verify your email
